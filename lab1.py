6a1421ee99623821ad15e998c640c6d4ee0a79bb
Alexandros Tsaparas <up1072824@upnet.gr>
2023-03-02 22:19:33 +0200

    Finished lab1.py

--- lab1.py
+++ lab1.py
@@ -1,4 +1,5 @@
 import requests
+import datetime
 
 url = input("Please enter a URL: ")
 
@@ -6,3 +7,16 @@ with requests.get(url) as response:
     print("\nResponse Headers:")
     for header, value in response.headers.items():
         print(f"{header}: {value}")
+    print("\nThe Web Server is running", response.headers['Server'])
+
+    if not response.cookies.get_dict():
+        print("The page does not use Cookies")
+    else:
+        print("The page also uses Cookies, such as:")
+        for cookie in response.cookies:
+            if cookie.expires == None:
+                print(f"{cookie.name}: Expired")
+            else:
+                expiration = datetime.datetime.fromtimestamp(cookie.expires)
+                print(f"{cookie.name}: Valid until {expiration}")
+        print("")
